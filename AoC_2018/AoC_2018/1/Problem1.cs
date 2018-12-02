dfe1c7cef0e5f903334cf334e75bc195633c4215
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-02 02:28:49 +0100

    Solve Problem 1_1

---
+++ AoC_2018/AoC_2018/1/Problem1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using FileParser;
+
+namespace AoC_2018._1
+{
+    class Problem1
+    {
+        const string _fileName = "1\\1_1.in";
+
+        internal void Solve()
+        {
+            ICollection<long> frequencyChanges = ParseInput(_fileName);
+
+            long result = 0;
+
+            foreach (long freq in frequencyChanges)
+            {
+                result += freq;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        internal ICollection<long> ParseInput(string inputFile)
+        {
+            ICollection<long> result = new List<long>();
+
+            IParsedFile parsedFile = new ParsedFile(inputFile);
+
+            while (!parsedFile.Empty)
+            {
+                IParsedLine parsedLine = parsedFile.NextLine();
+                while (!parsedLine.Empty)
+                {
+                    result.Add(parsedLine.NextElement<long>());
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AoC_2018/AoC_2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using AoC_2018._1;
+using System;
+
+namespace AoC_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Problem1 problem1 = new Problem1();
+            problem1.Solve();
+        }
+    }
+}
