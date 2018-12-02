be80713e52dffc581e325f47f0d7400097598f30
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-03 00:26:05 +0100

    Solve Problem 2_1

---
+++ AoC_2018/AoC_2018/2/Problem2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FileParser;
+
+namespace AoC_2018._2
+{
+    class Problem2
+    {
+        private readonly string _fileName = Path.Combine("2", "2.in");
+
+        internal void Solve_1()
+        {
+            ICollection<string> ids = ParseInput(_fileName);
+
+            long checkSum;
+            int stringsWithTwoRepeatedLetters = 0, stringsWithThreeRepeatedLetters = 0;
+
+            foreach (string id in ids)
+            {
+                Dictionary<char, int> charDic = new Dictionary<char, int>();
+                foreach (char ch in id)
+                {
+                    if (!charDic.TryAdd(ch, 1))
+                    {
+                        charDic[ch]++;
+                    }
+                }
+
+                stringsWithTwoRepeatedLetters += charDic.Values.Where(v => v == 2).Any() ? 1 : 0;
+                stringsWithThreeRepeatedLetters += charDic.Values.Where(v => v == 3).Any() ? 1 : 0;
+            }
+
+            checkSum = stringsWithTwoRepeatedLetters * stringsWithThreeRepeatedLetters;
+
+            Console.WriteLine($"Day 2, part 1: {checkSum}");
+        }
+
+        internal ICollection<string> ParseInput(string inputFile)
+        {
+            ICollection<string> result = new List<string>();
+
+            IParsedFile parsedFile = new ParsedFile(inputFile);
+
+            while (!parsedFile.Empty)
+            {
+                IParsedLine parsedLine = parsedFile.NextLine();
+                while (!parsedLine.Empty)
+                {
+                    result.Add(parsedLine.NextElement<string>());
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- AoC_2018/AoC_2018/Program.cs
+++ AoC_2018/AoC_2018/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AoC_2018._1;
+using AoC_2018._2;
 using System;
 
 namespace AoC_2018
@@ -7,12 +8,25 @@ namespace AoC_2018
     {
         static void Main(string[] args)
         {
+            SolveProblem2();
+
+            Console.ReadKey();
+        }
+
+        private static void SolveProblem1()
+        {
             Problem1 problem1 = new Problem1();
 
             problem1.Solve_1();
             problem1.Solve_2();
+        }
 
-            Console.ReadKey();
+        private static void SolveProblem2()
+        {
+            Problem2 problem = new Problem2();
+
+            problem.Solve_1();
+            //problem.Solve_2();
         }
     }
 }
