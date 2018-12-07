91f34fd1c8fcba78a69ff84cdeca709db61bdba8
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-07 02:30:26 +0100

    Improble Problem5 performance

--- AoC_2018/Solutions/Problem5.cs
+++ AoC_2018/Solutions/Problem5.cs
@@ -52,12 +52,28 @@ namespace AoC_2018.Solutions
             return a != b && char.ToUpperInvariant(a) == char.ToUpperInvariant(b);
         }
 
+        /// <summary>
+        /// Improvement:
+        /// Part 1: ~5.5s -> ~2.8s
+        /// Part 2: ~5.8s -> ~3.1s
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private bool EfficientIsReactionCondition(char a, char b)
+        {
+            return (a + _upperLowerCaseASCIIDifferenceValue == b)
+                || (b + _upperLowerCaseASCIIDifferenceValue == a);
+        }
+
+        private const uint _upperLowerCaseASCIIDifferenceValue = 32;
+
         private string CannotBelieveIveImplementedThisReact(string polymer)
         {
             Start:
             for (int index = 1; index < polymer.Length; ++index)
             {
-                if (IsReactionCondition(polymer[index - 1], polymer[index]))
+                if (EfficientIsReactionCondition(polymer[index - 1], polymer[index]))
                 {
                     polymer = polymer.Remove(index - 1, 2);
                     goto Start;
@@ -71,7 +87,7 @@ namespace AoC_2018.Solutions
         {
             for (int index = 1; index < polymer.Length; ++index)
             {
-                if (IsReactionCondition(polymer[index - 1], polymer[index]))
+                if (EfficientIsReactionCondition(polymer[index - 1], polymer[index]))
                 {
                     polymer = RecursiveReactThatCausesStackOverflow(polymer.Remove(index - 1, 2));
                 }
@@ -87,9 +103,15 @@ namespace AoC_2018.Solutions
 
             foreach (char unit in existingUnits)
             {
-                string modifiedPolymer = polymer
-                    .Replace(unit.ToString(), string.Empty)
-                    .Replace(unit.ToString().ToLowerInvariant(), string.Empty);
+                // Originally
+                //string modifiedPolymer = polymer
+                //    .Replace(unit, char.whi)
+                //    .Replace(unit.ToString().ToLowerInvariant(), string.Empty);
+
+                // https://stackoverflow.com/questions/2182459/fastest-way-to-remove-chars-from-string
+                string modifiedPolymer = string.Concat(
+                    polymer.Split(new char[] { unit, char.ToLowerInvariant(unit) },
+                    StringSplitOptions.None));
 
                 string result = CannotBelieveIveImplementedThisReact(modifiedPolymer);
 
