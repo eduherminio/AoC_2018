8f4133c5ccf45a751597525017afa2b7142091ba
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-23 21:10:46 +0100

    Solve Problem8_1

---
+++ AoC_2018/Model/Node.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC_2018.Model
+{
+    public class Node<TKey>
+    {
+        public TKey ParentId { get; set; }
+        public TKey Id { get; set; }
+
+        public HashSet<Node<TKey>> Children { get; set; }
+
+        public Node(TKey id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Node<TKey>))
+            {
+                return false;
+            }
+
+            return Equals((Node<TKey>)obj);
+        }
+
+        public bool Equals(Node<TKey> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (Id is string _id && other is Node<string> _other)
+            {
+                return _id == _other.Id;
+            }
+            else if (Id is int _id2 && other is Node<int> _other2)
+            {
+                return _id2 == _other2.Id;
+            }
+            else
+            {
+                throw new Exception("Wrong TKey in Node<TKey>");
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Node<TKey> node1, Node<TKey> node2)
+        {
+            if (ReferenceEquals(node1, null))
+            {
+                return ReferenceEquals(node2, null);
+            }
+
+            return node1.Equals(node2);
+        }
+
+        public static bool operator !=(Node<TKey> node1, Node<TKey> node2)
+        {
+            if (ReferenceEquals(node1, null))
+            {
+                return !ReferenceEquals(node2, null);
+            }
+
+            return !node1.Equals(node2);
+        }
+    }
+}
---
+++ AoC_2018/Solutions/Problem8.cs
@@ -0,0 +1,102 @@
+﻿using AoC_2018.Model;
+using FileParser;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AoC_2018.Solutions
+{
+    public class Problem8 : IProblem
+    {
+        public string FilePath => Path.Combine("Inputs", "8.in");
+
+        private HashSet<MetadataNode> _nodes = new HashSet<MetadataNode>();
+        Stack<int> _intStack;
+
+        public void Solve_1()
+        {
+            ParseInput();
+
+            long checkSum = 0;
+
+            foreach (int metadata in _nodes.SelectMany(node => node.Metadata))
+            {
+                checkSum += metadata;
+            }
+
+            Console.Write($"Day 8, part 1: {checkSum}");
+        }
+
+        public void Solve_2()
+        {
+            ParseInput();
+
+            int rootValue = ApplyPart2Algorithm();
+
+            Console.Write($"Day 8, part 1: {rootValue}");
+        }
+
+        private void ParseInput()
+        {
+            IParsedFile parsedFile = new ParsedFile(FilePath);
+
+            Stack<int> reversedStack = new Stack<int>(parsedFile.ToList<int>());
+            _intStack = new Stack<int>(reversedStack);
+
+            if (!parsedFile.Empty)
+            {
+                throw new Exception("Assumption of 1-line file is wrong");
+            }
+
+            ParseNodeTree();
+        }
+
+        private static int _id = -1;
+
+        private void ParseNodeTree()
+        {
+            while (_intStack.Any())
+            {
+                ParseNode();
+            }
+        }
+
+        private int ApplyPart2Algorithm()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParseNode()
+        {
+            ++_id;
+            Console.WriteLine(_id);
+
+            MetadataNode node = new MetadataNode(_id) { ParentId = _id - 1 };
+
+            int nChildNodes = _intStack.Pop();
+            int nMetadataEntries = _intStack.Pop();
+
+            for (int i = 0; i < nChildNodes; ++i)
+            {
+                ParseNode();
+            }
+
+            for (int i = 0; i < nMetadataEntries; ++i)
+            {
+                node.Metadata.Add(_intStack.Pop());
+            }
+
+            _nodes.Add(node);
+        }
+
+        private class MetadataNode : Node<int>
+        {
+            public ICollection<int> Metadata { get; set; } = new List<int>();
+
+            public MetadataNode(int id) : base(id) { }
+        }
+    }
+}
