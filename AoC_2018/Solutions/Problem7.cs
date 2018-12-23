7496493f6e284839696f1786571a265c5a98278b
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-23 18:15:44 +0100

    Solve Problem 7_2

--- AoC_2018/Solutions/Problem7.cs
+++ AoC_2018/Solutions/Problem7.cs
@@ -1,5 +1,4 @@
-﻿using AoC_2018.Model;
-using FileParser;
+﻿using FileParser;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -49,7 +48,100 @@ namespace AoC_2018.Solutions
 
         public void Solve_2()
         {
-            throw new NotImplementedException();
+            ICollection<Step> unorderedSteps = ParseInput();
+
+            int result = ApplyPart2Algorithm(unorderedSteps);
+
+            Console.Write($"Day 7, part 2: {result}");
+        }
+
+        private int ApplyPart2Algorithm(ICollection<Step> unorderedSteps)
+        {
+            string result = string.Empty;
+            const int totalAvailableWorkers = 5;     // Yourself and for Elves
+
+            IDictionary<int, Step> workersTimetable = new Dictionary<int, Step>(totalAvailableWorkers)
+            {
+                { 1, null },
+                { 2, null },
+                { 3, null },
+                { 4, null },
+                { 5, null }
+            };
+
+            int secondsSpent = 0;
+
+            while (unorderedSteps.Any())
+            {
+                ++secondsSpent;
+
+                AssignWorkers(ref unorderedSteps, ref workersTimetable);
+
+                SimulateOneSecond(ref result, ref unorderedSteps, ref workersTimetable);
+            }
+
+            return secondsSpent;
+        }
+
+
+        private IEnumerable<int> AvailableWorkers(IDictionary<int, Step> workersTimetable)
+        {
+            return workersTimetable.Where(pair => pair.Value == null).Select(tuple => tuple.Key);
+        }
+
+        private void AssignWorkers(ref ICollection<Step> unorderedSteps, ref IDictionary<int, Step> workersTimetable)
+        {
+            List<int> availableWorkers = AvailableWorkers(workersTimetable).ToList();
+
+            var assignableSteps = unorderedSteps
+                .Where(step => !step.InProgressOrCompleted)
+                .Where(step => !step.NonResolvedDependencies.Any())
+                .OrderBy(step => step.Name)
+                .ToList();
+
+            var stepsToAssign = assignableSteps.GetRange(0, Math.Min(assignableSteps.Count, availableWorkers.Count()));
+
+            for (int index = 0; index < stepsToAssign.Count(); ++index)
+            {
+                workersTimetable[availableWorkers.ElementAt(index)] = stepsToAssign.ElementAt(index);
+            }
+        }
+
+        private void SimulateOneSecond(ref string result, ref ICollection<Step> unorderedSteps, ref IDictionary<int, Step> workersTimetable)
+        {
+            List<int> workersIdleAfterSimulation = new List<int>();
+
+            foreach (var worker in workersTimetable.Where(pair => pair.Value != null))
+            {
+                Step step = unorderedSteps.Single(s => s == worker.Value);
+
+                if (step.NonResolvedDependencies.Any())
+                {
+                    throw new Exception(
+                        $"Exception in SimulateStep, step {step.Name} should not be assigned yet since it still has unresolved dependencies");
+                }
+
+                step.RunForOneSecond();
+
+                if (step.Completed)
+                {
+                    unorderedSteps.Remove(step);
+
+                    foreach (Step s in unorderedSteps)
+                    {
+                        s.NonResolvedDependencies.Remove(step);
+                    }
+
+                    workersIdleAfterSimulation.Add(worker.Key);
+
+                    result += step.Name;
+                }
+            }
+
+            foreach (int key in workersIdleAfterSimulation)
+            {
+                workersTimetable[key] = null;
+            }
         }
 
         private HashSet<Step> ParseInput()
@@ -65,7 +157,7 @@ namespace AoC_2018.Solutions
                 parsedLine.NextElement<string>();
 
                 string dependencyName = parsedLine.NextElement<string>();
-                Step stepDependency = new Step(dependencyName.Single());
+                Step stepDependency = new Step(char.ToUpperInvariant(dependencyName.Single()));
                 steps.Add(stepDependency);
 
                 while (!parsedLine.Empty)
@@ -76,7 +168,7 @@ namespace AoC_2018.Solutions
                     {
                         string mainStepName = parsedLine.NextElement<string>();
 
-                        if (steps.TryGetValue(new Step(mainStepName.Single()), out Step existingStep))
+                        if (steps.TryGetValue(new Step(char.ToUpperInvariant(mainStepName.Single())), out Step existingStep))
                         {
                             existingStep.NonResolvedDependencies.Add(stepDependency);
                         }
@@ -97,16 +189,28 @@ namespace AoC_2018.Solutions
 
             public HashSet<Step> NonResolvedDependencies { get; set; }
 
+            public bool InProgressOrCompleted => _restingDuration != (Name - 4);
+
+            public bool Completed => _restingDuration == 0;
+
+            private int _restingDuration;
+
             public Step(char name)
             {
                 Name = name;
                 NonResolvedDependencies = new HashSet<Step>();
+                _restingDuration = name - 4;
             }
 
             public Step(char name, Step dependency)
+                : this(name)
             {
-                Name = name;
                 NonResolvedDependencies = new HashSet<Step> { dependency };
+            }
+
+            public int RunForOneSecond()
+            {
+                return --_restingDuration;
             }
 
             public override bool Equals(object obj)
