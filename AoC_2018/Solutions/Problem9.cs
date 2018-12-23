8397cea06a5248faa869592bc8e2aeb3359e0747
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-12-24 00:38:41 +0100

    Start Problem9_1

---
+++ AoC_2018/Solutions/Problem9.cs
@@ -0,0 +1,105 @@
+﻿using FileParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AoC_2018.Solutions
+{
+    public class Problem9 : IProblem
+    {
+        public string FilePath => Path.Combine("Inputs", "9.in");
+
+        public void Solve_1()
+        {
+            var input = ParseInput();
+            //int numberOfPlayers = input.Item1;
+            //int numberOfMarbles = input.Item2;
+
+            //int numberOfPlayers = 9;
+            //int numberOfMarbles = 25;
+
+            int numberOfPlayers = 10;
+            int numberOfMarbles = 1618;
+
+            Dictionary<int, int> playerPointPairs = new Dictionary<int, int>(numberOfMarbles);
+            for (int i = 0; i < numberOfPlayers; ++i)
+            {
+                playerPointPairs.Add(i, 0);
+            }
+
+            LinkedList<int> field = new LinkedList<int>();
+
+            field.AddFirst(0);
+            field.AddLast(1);
+            int turn = 0;
+            int indexOfCurrentMarble = 1;
+
+            for (int marbleIndex = 2; marbleIndex <= numberOfMarbles; ++marbleIndex)
+            {
+                ++turn;
+                turn %= numberOfPlayers;    // 1 - numberOfPlayers
+
+                if (marbleIndex % 23 == 0)
+                {
+                    int indexOfMarbleToRemove =
+                        (indexOfCurrentMarble - 7) > 0
+                        ? indexOfCurrentMarble - 7
+                        : (field.Count - 1) + (indexOfCurrentMarble - 7);
+
+                    playerPointPairs[turn] += marbleIndex + field.ElementAt(indexOfMarbleToRemove);
+
+                    indexOfCurrentMarble = indexOfMarbleToRemove;
+
+                    field.Remove(field.Find(field.ElementAt(indexOfMarbleToRemove)));
+                }
+                else
+                {
+                    if (indexOfCurrentMarble + 2 <= field.Count)
+                    {
+                        field.AddAfter(field.Find(field.ElementAt(indexOfCurrentMarble + 1)), marbleIndex);
+                        indexOfCurrentMarble += 2;
+                    }
+                    else if (indexOfCurrentMarble + 2 == field.Count + 1)   // Last one
+                    {
+                        field.AddAfter(field.First, marbleIndex);
+                        indexOfCurrentMarble = 1;
+                    }
+                    else
+                    {
+                        throw new Exception(":(");
+                    }
+                }
+            }
+
+            int winner = playerPointPairs.Max(pair => pair.Value);
+
+            Console.Write($"Day 9, part 1: {winner}");
+        }
+
+        public void Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int> ParseInput()
+        {
+            IParsedFile parsedFile = new ParsedFile(FilePath);
+
+            IParsedLine parsedLine = parsedFile.NextLine();
+
+            int nPlayers = parsedLine.NextElement<int>();
+
+            string str = string.Empty;
+            do
+            {
+                str = parsedLine.NextElement<string>();
+            } while (str != "worth");
+
+            int maxPoints = parsedLine.NextElement<int>();
+
+            return Tuple.Create(nPlayers, maxPoints);
+        }
+    }
+}
